6add6ee7de23b1778d1121fcd13889b8e78339c0
mwinb <mwinberry0101@gmail.com>
2019-02-14 19:44:41 -0800

    CREATE DocumentReader Class to hand opening and reading files

---
+++ SourceCode/DocumentReader.py
@@ -0,0 +1,21 @@
+class LineReader:
+    lines = []
+
+    def __init__(self, path):
+        self.lines = self.read_lines(path)
+
+    def get_lines(self):
+        return self.lines
+    
+    def set_lines(self, path):
+        self.lines = self.read_lines(path)
+
+    def read_lines(self, path):
+        try:
+            lines = []
+            with open(path, 'r+') as f:
+                for line in f.readlines():
+                    lines.append(line)
+            return lines
+        except Exception:
+            return []
